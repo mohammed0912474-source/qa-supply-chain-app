0d6317e0f0724ddab240987a6102c89a51aa7fce
mohammed0912474-source <mohammed0912474@gmail.com>
2026-07-25 09:22:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* غيّر رقم الإصدار ده مع كل نسخة جديدة تنزلها (لازم يتغيّر عشان كل الأفراد ياخدوا التحديث) */
-const CACHE_NAME = 'qa-supply-chain-v3.3.0';
+const CACHE_NAME = 'qa-supply-chain-v3.4.0-debug';
 const APP_SHELL = [
   './',
   './index.html',
